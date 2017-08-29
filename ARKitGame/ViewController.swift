caccc7345f4bdd287b6e08119eab5ee008a46583
Tyler Jaacks <TylerJaacks@Gmail.com>
2017-08-28 23:22:05 -0500

    Fixed configuration variable to conform to the new ARKit spec.

--- ARKitGame/ViewController.swift
+++ ARKitGame/ViewController.swift
@@ -79,7 +79,7 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        let configuration = ARWorldTrackingSessionConfiguration()
+        let configuration = ARWorldTrackingConfiguration()
         
         sceneView.session.run(configuration)
         
